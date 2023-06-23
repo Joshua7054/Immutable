649b30acaf77d77008095f0852010f4dea9b8643
joshua.cho <joshua.cho@toss.im>
2023-06-23 12:39:16 +0900

    inserting & deleting 제거

--- Sources/Immutable/Collection.swift
+++ Sources/Immutable/Collection.swift
@@ -20,39 +20,4 @@ extension RangeReplaceableCollection {
     copy.append(contentsOf: newElements)
     return copy
   }
-
-  /// Returns a new collection by inserting a new element.
-  public func inserting(_ newElement: Self.Iterator.Element, at i: Self.Index) -> Self {
-    var copy = self
-    copy.insert(newElement, at: i)
-    return copy
-  }
-
-  /// Returns a new collection by inserting new elements.
-  public func inserting<C>(contentsOf newElements: C, at i: Self.Index) -> Self where C: Collection, C.Iterator.Element == Self.Iterator.Element {
-    var copy = self
-    copy.insert(contentsOf: newElements, at: i)
-    return copy
-  }
-
-  /// Returns a new collection by removing an element at specified index.
-  public func removing(at i: Self.Index) -> Self {
-    var copy = self
-    copy.remove(at: i)
-    return copy
-  }
-
-  /// Returns a new collection by removing all elements.
-  public func removingAll(keepingCapacity: Bool = false) -> Self {
-    var copy = self
-    copy.removeAll(keepingCapacity: keepingCapacity)
-    return copy
-  }
-
-  /// Returns a new collection by removing first element.
-  public func removingFirst(_ n: Int = 1) -> Self {
-    var copy = self
-    copy.removeFirst(n)
-    return copy
-  }
 }
