52277562ba3b100be855a3f86588005e28dfbac4
joshua.cho <joshua.cho@toss.im>
2023-07-05 15:48:14 +0900

    Dictionary의 map, flatMap제거

--- Sources/Immutable/Dictionary.swift
+++ Sources/Immutable/Dictionary.swift
@@ -7,24 +7,6 @@
 //
 
 extension Dictionary {
-  /// Initialize dictionary with a collection of key-value tuples.
-  public init<C>(elements: C) where C: Collection, C.Iterator.Element == (Key, Value) {
-    self.init()
-    for (key, value) in elements {
-      self[key] = value
-    }
-  }
-
-  /// `map()` that returns `Dictionary`.
-  public func map<T, U>(_ transform: (Key, Value) throws -> (T, U)) rethrows -> [T: U] where T: Hashable {
-    return Dictionary<T, U>(elements: try self.map(transform))
-  }
-
-  /// `flatMap()` that returns `Dictionary`.
-  public func flatMap<T, U>(_ transform: (Key, Value) throws -> (T, U)?) rethrows -> [T: U] where T: Hashable {
-    return Dictionary<T, U>(elements: try self.compactMap(transform))
-  }
-
   /// returns a new directory by merging all values for respective keys.
   public func merging(_ dictionary: Dictionary<Key, Value>) -> Dictionary<Key, Value> {
     var copy = self
